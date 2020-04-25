8c693f095d6b289034ab6323ac460be53b8479f1
memalette <marieeve.malette@gmail.com>
2020-04-24 21:44:44 -0400

    real envirnonment

--- index_environment.py
+++ index_environment.py
@@ -4,102 +4,98 @@ import matplotlib.pyplot as plt
 
 
 class Env:
-	def __init__(self, n_assets, s0, mus, sigmas, T, epsilon_param):
-		self.n_assets = n_assets
-		self.S = s0
-		self.mus = mus
-		self.sigmas = sigmas
-		self.T = T
-		self.epsilon_param = epsilon_param
+	def __init__(self, max_diff=None, leverage_cost=None, trans_cost=None, 
+					max_leverage=None, cutoff_leverage=None):
+		self.max_diff = max_diff
+		self.max_leverage = max_leverage
+		self.leverage_cost = leverage_cost
+		self.trans_cost = trans_cost
+		self.cutoff_leverage = cutoff_leverage
 
 		self.reset()
 
+		self.returns = pd.read_csv('returns.csv', index_col=0)
+		self.index_col = self.returns.columns[0]
+		self.assets_col = self.returns.columns[1:]
+		self.n_assets = len(self.assets_col)
 
-	def step_betas(self):
-		constraint_respected = False
-		while not constraint_respected:
-			epsilon = np.random.normal(self.epsilon_param[0], self.epsilon_param[1], size=self.n_assets)
-			next_betas = self.betas + epsilon
-			next_betas = next_betas/np.sum(next_betas)
+		self.t = 0
+		self.T = 500 #self.returns.shape[0]  # 500
 
-			if max(np.abs(next_betas)) < 2: 
-				constraint_respected = True
-
-		self.betas = next_betas
-
-
-	def init_betas(self):
-		constraint_respected = False
-		while not constraint_respected:
-			betas = np.random.uniform(-1, 1, size=self.n_assets)
-			betas = betas /np.sum(betas)
-
-			if max(np.abs(betas)) < 2:
-				constraint_respected = True
-
-		self.betas = betas
-
-
-	def step_index(self):
-		self.step_betas()
-		self.step_assets()
-
-		return np.dot(self.assets.T, self.betas)
-
-
-	def step_assets(self):
-		dT = 1
-		Z = np.random.normal(0,1, size = self.n_assets)
-		prev_S = self.S
-		self.S = prev_S + (self.mus-0.5*self.sigmas**2)*dT + self.sigmas*np.sqrt(dT)*Z
-		self.assets = (self.S-prev_S)/prev_S
-
+		self.reset()
 
 	def reset(self):
-		self.init_betas()
-		self.state = np.random.normal(0, 0.2, size=1)
+		# reset values
+		self.t = 0
+		self.state = np.array([0, 0, 0])
+		self.prev_action = 0
+
+		# logging lists
+		self.portfolio_returns = []
+		self.index_returns = []
+		
 		return self.state
 
-	def get_reward(self):
-		return (self.state)**2
+	def step_index(self):
+		return self.returns[self.index_col].values[self.t]
 
-	def step(self, action):
-		index = self.step_index()
-		self.portfolio = np.dot(self.assets.T, action)
-		self.state = index - self.portfolio
-
-		reward = self.get_reward()
-		done = False # continuous task
-
-		return self.state, reward, done
+	def step_assets(self):
+		return self.returns[self.assets_col].values[self.t,:]
 
 
-
-if __name__ == '__main__':
-
-    N_ASSETS = 3 
-    EPSILON_PARAM = (0, 0.01)
-    T = 252 # trading days in a year (this could be the end of an episode)
-    S0 = np.array([10, 15, 20])
-    MUS = np.array([-0.01, 0.02, 0.05])
-    SIGMAS = np.array([0.01, 0.05, 0.10])
-
-    env = Env(N_ASSETS, S0, MUS, SIGMAS, T, EPSILON_PARAM)
+	def get_reward(self, state):
+		leverage_cost = -self.leverage_cost if self.state[2] > self.cutoff_leverage else 0
+		leverage_cost += -0.05 if self.state[2] > self.max_leverage else 0
+		reward = -(self.state[0])**2 - self.state[1]*self.trans_cost - leverage_cost
+		return reward.flatten()
 
 
-    # example of a simulation for a portfolio that stays constant
-    action = np.array([1./3., 1./3., 1./3.])
-    portfolio_returns = []
-    for step in range(T):
-        env.step(action)
-        portfolio_returns.append(env.portfolio)
+	def step(self, action=None, delta=None):
+		# step index - get index return
+		self.index = self.step_index()
+		self.index_returns.append(float(self.index))
 
+		# step assets - get assets returns
+		self.assets = self.step_assets()
+		self.t += 1
 
-    # transform returns into cumulative values
-    portfolio_value = np.cumprod(np.array(portfolio_returns)+1)
+		# if for particle filter
+		if (action is not None) and (delta is not None):
+			# get portfolio return
+			self.portfolio = np.dot(action, self.assets.reshape((self.n_assets,1)))
 
+			# state 0 - difference between index and portfolio
+			difference_indicator = float(self.index - self.portfolio)
 
-    plt.plot(portfolio_value)
-    plt.show()
+			# state 1 - transaction cost indicator
+			transaction_indicator = float(np.sum(np.abs(delta)))
 
+			# state 2 - gross leverage indicator 
+			leverage_indicator = float(np.sum(np.abs(action)))
 
+			# store in new state
+			self.state = np.array([difference_indicator, transaction_indicator, leverage_indicator])
+
+			# get reward
+			reward = self.get_reward(self.state)
+
+			# store values for next iteration
+			self.portfolio_returns.append(float(self.portfolio))
+			self.prev_action = action
+
+			# stopping rule
+			rule1 = (np.abs(self.state[0]) > self.max_diff)
+			rule2 = (self.t == self.T) 
+			rule3 = (self.state[2] > self.max_leverage)
+
+			if rule1 or rule2 or rule3:
+				done = True
+			else:
+				done = False
+
+			return self.state.flatten(), reward, done
+
+		else: 
+
+			done = True if (self.t == self.T) else False
+			return done
---
+++ particle_filter.py
@@ -0,0 +1,121 @@
+import numpy as np
+import math as math
+from scipy.stats import *
+from numpy.random import random
+from index_environment import *
+
+class ParticleFilter:
+	def __init__(self, n_particles, n_assets, max_gross_levergage, vol, likeli_scale):
+		self.n_particles = n_particles
+		self.n_assets = n_assets
+		self.max_gross_levergage = max_gross_levergage
+		self.vol = vol
+		self.likeli_scale = likeli_scale
+
+		self.q = np.zeros((self.n_assets, self.n_assets))
+		np.fill_diagonal(self.q, self.vol)
+
+	def initialize_action(self):
+		action = np.random.uniform(0, 1, (self.n_particles, self.n_assets))
+		sums = np.sum(action, axis=1).reshape(action.shape[0],1)
+		div =  np.tile(sums, self.n_assets) 
+		action = action/div
+
+		return action
+
+
+	def multinomial_resample(self, weights):
+		cumul_sum = np.cumsum(weights)
+		cumul_sum[-1] = 1.
+		return np.searchsorted(cumul_sum, random(len(weights)))
+
+
+	def learn(self, env):
+
+		state = env.reset()
+		action_logs = []
+		portfolio_returns = []
+
+		action = self.initialize_action()
+		terminal = False
+		i=0
+		while not terminal:
+			epsilon = np.random.multivariate_normal(mean=np.array([0.0]*self.n_assets), 
+													cov=self.q, 
+													size=self.n_particles
+												)
+			action_pres = action + epsilon
+			sums = np.sum(action_pres, axis=1).reshape(action_pres.shape[0],1)
+			div =  np.tile(sums, self.n_assets)  
+			action_pres = action_pres/div
+			
+
+			# get state
+			terminal = env.step()
+			states = np.sum(action_pres * env.assets, axis=1)
+
+
+			likelihood = norm.pdf(x=states-env.index, loc=0, scale=self.likeli_scale)
+
+			idx = self.multinomial_resample(likelihood/np.sum(likelihood))
+			action_post = action_pres[idx]
+
+			action = action_post.mean(0)
+			portfolio = np.dot(action.T, env.assets.reshape((self.n_assets,1)))
+			print(portfolio - env.index)
+			portfolio_returns.append(portfolio)
+			action_logs.append(action)
+
+			i+=1
+
+		return action_logs, env.index_returns, portfolio_returns
+
+
+
+if __name__ == '__main__':
+
+	N_PARTICLES = 1000000 # the more the merrier, but it gets slow
+	MAX_GROSS_LEVERGAE = 2.0 # currently, Im not putting any restrictions on this
+	VOL = 0.1 # standard deviation for the epsilon 
+	LIKELI_SCALE = 0.005 # the smaller, the better it will track the index
+
+	# instantiate index environment
+	env = Env()
+
+	particle_filter_agent = ParticleFilter(
+								n_particles=N_PARTICLES, 
+								n_assets=env.n_assets, 
+								max_gross_levergage=MAX_GROSS_LEVERGAE, 
+								vol=VOL, 
+								likeli_scale = LIKELI_SCALE
+								)
+
+	action_logs, index_returns, portfolio_returns = particle_filter_agent.learn(env)
+
+	action_logs = np.vstack(action_logs)
+	gross_levergage = action_logs.sum(1)
+
+	plt.plot(gross_levergage)
+	plt.title('Replication Portfolio gross leverage')
+	plt.show()
+
+	plt.close()
+	plt.plot(np.array(index_returns).flatten())
+	plt.plot(np.array(portfolio_returns).flatten())
+	plt.title('Index vs Replication Portfolio returns')
+	plt.show()
+
+
+	# unit values
+	index_returns = np.array(index_returns).flatten()
+	portfolio_returns = np.array(portfolio_returns).flatten()
+	index_value = np.cumprod(1 + index_returns)
+	portfolio_value = np.cumprod(1 + portfolio_returns)
+
+
+	plt.close()
+	plt.plot(index_value, label='index')
+	plt.plot(portfolio_value, label='portfolio')
+	plt.title('Index vs Replication Portfolio values')
+	plt.legend()
+	plt.show()
