93a5f93846fbb7a77282d2e682474e4af4162dc5
Marie-Ève <marieeve.malette@gmail.com>
2020-04-28 15:30:07 -0400

    changes to A2C and env

--- agents/actor_critic.py
+++ agents/actor_critic.py
@@ -1,41 +1,45 @@
 # Necassary import statments
+from index_environment import *
 import numpy as np
 
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
+import torch.nn.functional as F
 
+
+import matplotlib
 import matplotlib.pyplot as plt
+import seaborn as sns
 from collections import namedtuple
 from tqdm.auto import tqdm
+import glob
 
-from agents.base import Base
-from environments.index_environment import *
-from utils.utils import Exp
 
 
 class ValueEstimator(nn.Module):
 
-    def __init__(self, n_inputs, n_hidden=128, lr=0.00001):
+    def __init__(self,n_inputs, n_hidden = 128,lr = 0.00001):
         
-        super(ValueEstimator, self).__init__()
+        super(ValueEstimator,self).__init__()
         
         # Model definition
         self.model = nn.Sequential(
-            nn.Linear(n_inputs, n_hidden),
+            nn.Linear(n_inputs,n_hidden),
             nn.ReLU(),
             # nn.Linear(n_hidden,n_hidden),
             # nn.ReLU(),
-            nn.Linear(n_hidden, 1)
+            nn.Linear(n_hidden,1)
         )
        
         # Model optimizer
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr)
+        self.optimizer = torch.optim.Adam(self.model.parameters(),lr)
 
         # Loss criterion
         self.criterion = torch.nn.MSELoss()
 
-    def predict(self, state):
+
+    def predict(self,state):
         """
         Compute the probability for each action corresponding to state s
         """
@@ -43,36 +47,44 @@ class ValueEstimator(nn.Module):
         with torch.no_grad():
             return self.model(state)
 
-    def update(self, states, returns):
+    def update(self,states,returns):
         """
         Update the weights of the network based on 
             states     : input states for the value estimator
             returns    : actual monte-carlo return for the states
         """
         pred_returns = self.model(states)
-        loss = self.criterion(pred_returns, Variable(returns))
+        loss = self.criterion(pred_returns,Variable(returns))
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
         # print('loss value estimator: '+str(loss))
 
         return float(loss.detach().numpy())
+        
+
+class Exp(nn.Module):
+
+    def __init__(self):
+        super(Exp, self).__init__()
+
+    def forward(self, x):
+        out = torch.exp(x)
+        out = torch.clamp(out, min=0.0001)
+        return out
 
 
 class PolicyNN(nn.Module):
-    def __init__(self, n_inputs, n_outputs, n_hidden=128):
-        super(PolicyNN, self).__init__()
+    def __init__(self,n_inputs, n_outputs, n_hidden = 128):
+        super(PolicyNN,self).__init__()
 
         # Model definition
         # first layer
         self.l1 = nn.Sequential(
-            nn.Linear(n_inputs, n_hidden),
+            nn.Linear(n_inputs,n_hidden),
             #nn.Dropout(0.6),
             nn.ReLU(),
 
-            # nn.Linear(n_hidden,n_hidden),
-            # #nn.Dropout(0.6),
-            # nn.ReLU()
         )
 
         self.alpha = nn.Sequential(
@@ -80,33 +92,29 @@ class PolicyNN(nn.Module):
             Exp()
         )
 
-    def forward(self, state):
-        """
-        Compute the probability for each action corresponding to state s
-        """
+    def forward(self,state):
         l1_output = self.l1(state)
         return self.alpha(l1_output) 
 
 
+
 class PolicyEstimator(nn.Module):
-    def __init__(self, policy_nn, lr=0.0001):
-        super(PolicyEstimator, self).__init__()
+    def __init__(self, policy_nn,lr = 0.0001):
+        super(PolicyEstimator,self).__init__()
 
         self.policy_nn = policy_nn
        
         # Model optimizer
-        self.optimizer = torch.optim.Adam(self.policy_nn.parameters(), lr)
+        self.optimizer = torch.optim.Adam(self.policy_nn.parameters(),lr)
 
-    def predict(self, state):
 
-        """
-        Compute the probability for each action corresponding to state s
-        """
+    def predict(self,state):
+
         state = torch.tensor(state, dtype=torch.float)
         
         return self.policy_nn(state)
 
-    def update(self, advantages, log_probs):
+    def update(self,advantages,log_probs):
         """
         Update the weights of the network based on 
             advantages : advantage for each step in the episode
@@ -115,7 +123,7 @@ class PolicyEstimator(nn.Module):
 
         policy_gradients = []
 
-        for log_prob, adv in zip(log_probs, advantages):
+        for log_prob,adv in zip(log_probs,advantages):
             policy_gradients.append(-log_prob * adv)
 
         # Computing gradient ascent using negative loss 
@@ -132,7 +140,9 @@ class PolicyEstimator(nn.Module):
     def sample_action(self, dirichlet_dist):
         return dirichlet_dist.sample()
 
-    def select_action(self, state):
+
+    def select_action(self,state):
+
         alpha = self.predict(state)
         # print(alpha)
 
@@ -146,13 +156,18 @@ class PolicyEstimator(nn.Module):
         return action, log_prob
 
 
-class ActorCritic(Base):
-    def __init__(self,n_episodes, gamma, lr_valf, lr_pol):
+
+
+
+class ActorCritic:
+    def __init__(self,n_episodes, gamma, lr_valf, lr_pol, n_hidden_valf, n_hidden_pol):
 
         self.n_episodes = n_episodes
         self.gamma = gamma
         self.lr_valf = lr_valf
         self.lr_pol = lr_pol
+        self.n_hidden_valf = n_hidden_valf
+        self.n_hidden_pol = n_hidden_pol
 
     def learn(self, env):
         # stats for the episode
@@ -160,10 +175,10 @@ class ActorCritic(Base):
         episodic_length = []
 
         # Define value function approximator
-        valf_est = ValueEstimator(env.n_states, lr=self.lr_valf) 
+        valf_est = ValueEstimator(n_inputs=env.n_states, lr=self.lr_valf, n_hidden=self.n_hidden_valf) 
 
         # Define policy estimator
-        policy_nn = PolicyNN(n_inputs=env.n_states, n_outputs=env.n_assets)
+        policy_nn = PolicyNN(n_inputs=env.n_states, n_outputs=env.n_assets, n_hidden=self.n_hidden_pol)
         policy_est = PolicyEstimator(policy_nn, lr=self.lr_pol) 
         loss_valf = []
         loss_pol = []
@@ -190,7 +205,6 @@ class ActorCritic(Base):
                 # Choose action based on the state and current parameter to generate a full episode
                 action, log_prob = policy_est.select_action(state)
                 delta = action - prev_action
-
                 # print(action)
                 action_logs.append(action.numpy())
                 log_probs.append(log_prob)
@@ -263,16 +277,18 @@ class ActorCritic(Base):
                         loss_pol.pop(0)
 
                         if current_mean_valf < best_loss_valf:
-                            torch.save(valf_est.state_dict(), '../models/actor_critic/best_valf_est.pt')
+                            torch.save(valf_est.state_dict(), 'models/best_valf_est.pt')
                             best_loss_valf = current_mean_valf
 
                         if current_mean_pol < best_loss_pol: 
                             last_best = eps
-                            torch.save(policy_est.state_dict(), '../models/actor_critic/best_pol_est.pt')
+                            torch.save(policy_est.state_dict(), 'models/best_pol_est.pt')
                             best_loss_pol = current_mean_pol
 
                         if last_best - eps > 20:
                             break
+
+
 
     def predict(self, env, pred_id=None):
 
@@ -290,12 +306,12 @@ class ActorCritic(Base):
 
         # Define value function approximator
         valf_est = ValueEstimator(env.n_states) 
-        valf_est.load_state_dict(torch.load('../models/actor_critic/best_valf_est.pt'))
+        valf_est.load_state_dict(torch.load('models/best_valf_est.pt'))
 
         # Define policy estimator
         policy_nn = PolicyNN(n_inputs=env.n_states, n_outputs=env.n_assets)
         policy_est = PolicyEstimator(policy_nn) 
-        policy_est.load_state_dict(torch.load('../models/actor_critic/best_pol_est.pt'))
+        policy_est.load_state_dict(torch.load('models/best_pol_est.pt'))
 
 
         while not terminal:
@@ -310,26 +326,29 @@ class ActorCritic(Base):
             next_state = None if terminal else next_state
             prev_action = action
 
-            portfolio = np.dot(action.numpy(), env.assets.reshape((env.n_assets, 1)))
+            portfolio = np.dot(action.numpy(), env.assets.reshape((env.n_assets,1)))
             portfolio_returns.append(portfolio)
 
             # update the total length for this episode
             T += 1
 
             state = next_state
+              
 
         action_logs = pd.DataFrame(np.vstack(action_logs)[:,:2], index=env.dates)
 
         plt.plot(action_logs)
-        plt.savefig('../figs/action_logs'+str(pred_id)+'.png')
+        plt.savefig('figs/action_logs'+str(pred_id)+'.png')
         plt.close()
+
 
         returns_logs = pd.DataFrame({'index': np.array(env.index_returns).flatten(), 
                                      'replicator': np.array(portfolio_returns).flatten()},
                                       index=env.dates)
         returns_logs.plot(marker='.')
-        plt.savefig('../figs/returns'+str(pred_id)+'.png')
+        plt.savefig('figs/returns'+str(pred_id)+'.png')
         plt.close()
+
 
         # unit values
         index_returns = np.array(env.index_returns).flatten()
@@ -343,12 +362,14 @@ class ActorCritic(Base):
         
         values_logs.plot(marker='.')
         plt.legend()
-        plt.savefig('../figs/values'+str(pred_id)+'.png')
+        plt.savefig('figs/values'+str(pred_id)+'.png')
         plt.close()
+
 
         tracking_errors = (returns_logs['index'] - returns_logs['replicator'])**2
 
         return tracking_errors.mean()
+
 
 
 if __name__ == '__main__':
@@ -358,18 +379,21 @@ if __name__ == '__main__':
     N_EPISODES = 1000
     LR_POL = 0.0001
     LR_VALF = 0.0001
+    EXP = 3
+    N_HIDDEN_POL = 400
+    N_HIDDEN_VALF = 400
     
     Transition = namedtuple('Transition',('state','action','reward','next_state'))
 
     # train
-    # env = Env(context='train')
-    # actor_critic_agent = ActorCritic(N_EPISODES, GAMMA, LR_VALF, LR_POL)
-    # actor_critic_agent.learn(env)
+    env = Env(context='train', experiment=EXP)
+    actor_critic_agent = ActorCritic(N_EPISODES, GAMMA, LR_VALF, LR_POL, N_HIDDEN_VALF, N_HIDDEN_POL)
+    actor_critic_agent.learn(env)
 
 
     # test
-    env = Env(data_path='../returns.csv', context='test')
-    actor_critic_agent = ActorCritic(N_EPISODES, GAMMA, LR_VALF, LR_POL)
+    env = Env(context='test', experiment=EXP)
+    actor_critic_agent = ActorCritic(N_EPISODES, GAMMA, LR_VALF, LR_POL, N_HIDDEN_VALF, N_HIDDEN_POL)
 
     TE = []
     for i in range(10):
--- environments/index_environment.py
+++ environments/index_environment.py
@@ -1,18 +1,18 @@
 import numpy as np
 import pandas as pd
-import pdb
+import matplotlib.pyplot as plt
 
 
 class Env:
-	def __init__(self, data_path, context='train'):
+	def __init__(self, context='train', experiment=0):
 
-		self.returns = pd.read_csv(data_path, index_col=0)
+		self.returns = pd.read_csv('returns.csv', index_col=0)
 		half = int(self.returns.shape[0]/2)
 
 		if context == 'train':
-			self.returns.iloc[:half, :]
+			self.returns.iloc[:half,:]
 		elif context == 'test':
-			self.returns.iloc[half:, :]
+			self.returns.iloc[half:,:]
 
 		self.index_col = self.returns.columns[0]
 		self.assets_col = self.returns.columns[1:]
@@ -21,24 +21,37 @@ class Env:
 		self.t = 0
 		self.T = 100
 		self.history_len = self.returns.shape[0]  
+		self.experiment = experiment
 
 		self.reset()
 
-	def reset(self):
 
+	def reset(self):
 		# reset values
 		self.start = int(np.random.uniform(0, self.history_len-self.T))
 		self.t = 0
-		self.state = np.array([0]*(self.n_assets+2))
-		self.n_states = self.state.shape[0]
+		
 		self.prev_action = 0
 		self.IV = 1
 		self.PV = 1
+		self.AV = np.array([1]*self.n_assets)
 		self.dates = self.returns.index[self.start:(self.start+self.T)]
 
 		# logging lists
 		self.portfolio_returns = []
 		self.index_returns = []
+
+		if self.experiment == 0:
+			self.state = np.array([0]*(self.n_assets+1))
+		elif (self.experiment == 1):
+			self.state = np.array([0]*(self.n_assets+2))
+		elif (self.experiment == 2):
+			self.state = np.array([0]*(3*self.n_assets+2))
+		elif (self.experiment == 3):
+			self.state = np.array([0]*(3*self.n_assets+2))
+
+		self.n_states = self.state.shape[0]
+
 		
 		return self.state
 
@@ -48,12 +61,29 @@ class Env:
 		return index
 
 	def step_assets(self):
-		return self.returns[self.assets_col].values[self.t+self.start, :]
+		assets = self.returns[self.assets_col].values[self.t+self.start,:]
+		self.AV = self.AV * (1+assets)
+		return assets
+
 
 	def get_reward(self, state):
-		# total_delta = np.sum(np.abs(state[2:]))
-		reward = -np.abs(self.state[0]) - np.abs(self.state[1])*5 #- total_delta*0.02
+	
+		base_penalty = -np.abs(self.state[0])
+
+		if self.experiment == 0:
+			reward = base_penalty
+		elif (self.experiment == 1) or (self.experiment == 2):
+			cumul_penalty = -np.abs(self.state[1])*5
+			reward = base_penalty + cumul_penalty
+		elif (self.experiment == 3): 
+			cumul_penalty = -np.abs(self.state[1])*5
+			turnover_penalty = -np.sum(np.abs(state[2:17]))*0.7
+			reward = base_penalty + cumul_penalty + turnover_penalty
+		else: 
+			print('Specify experiment 0-4')
+
 		return reward.flatten()
+
 
 	def step(self, action=None, delta=None):
 		# step index - get index return
@@ -65,17 +95,28 @@ class Env:
 		self.t += 1
 
 		# if for particle filter
-		if action is not None:
+		if (action is not None):
 			# get portfolio return
-			self.portfolio = np.dot(action, self.assets.reshape((self.n_assets, 1)))
-			self.PV = self.PV * (1+self.portfolio)
+			self.portfolio = np.dot(action, self.assets.reshape((self.n_assets,1)))
+			self.PV = self.PV *(1+self.portfolio)
 
 			# state 0 - difference between index and portfolio
 			difference_indicator1 = float(self.index - self.portfolio)
 			difference_indicator2 = float(self.IV - self.PV)
+			cumulative_returns = self.AV - 1
+			past_returns = self.assets
 
 			# store in new state
-			self.state = np.concatenate((np.array([difference_indicator1, difference_indicator2]), delta))
+			if self.experiment == 0:
+				self.state = np.concatenate((np.array([difference_indicator1]), delta))
+			elif (self.experiment == 1):
+				self.state = np.concatenate((np.array([difference_indicator1, difference_indicator2]), delta))
+			elif (self.experiment == 2):
+				self.state = np.concatenate((np.array([difference_indicator1, difference_indicator2]), delta, 
+										cumulative_returns, past_returns))
+			elif (self.experiment == 3):
+				self.state = np.concatenate((np.array([difference_indicator1, difference_indicator2]), delta, 
+										cumulative_returns, past_returns))
 
 			# get reward
 			reward = self.get_reward(self.state)
